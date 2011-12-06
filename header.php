cd3015f8e33d1b2c42b68792fdc4a974174ccc4b
Zane M. Kolnik <zanematthew@gmail.com>
2011-12-06 16:45:37 -0500

    first

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<?php wp_head(); ?>
+</head>
+</html>
+<body>
+ <!-- Topbar
+    ================================================== -->
+    <div class="topbar" data-scrollspy="scrollspy" >
+      <div class="topbar-inner">
+        <div class="container-fluid">
+          <a class="brand" href="#">BMX Race Schedules</a>
+          <ul class="nav">
+            <li class="active"><a href="#overview">Overview</a></li>
+            <li><a href="#about">About</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <header class="masthead">
+      
+    </header>
+</body>
